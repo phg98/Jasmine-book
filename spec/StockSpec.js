2e9156022602744cbbbe5afd714c5cd6709bae0f
Harry Park <hg98.park@hanwha.com>
2019-01-20 00:30:12 +0900

    #8 sinonJS

--- spec/StockSpec.js
+++ spec/StockSpec.js
@@ -1,4 +1,5 @@
 describe('주식이 조회되면', () => {
+    var xhr;
     var originalSharePrice = 10;
     var stock = new Stock({
         symbol: 'AOUE',
@@ -6,10 +7,29 @@ describe('주식이 조회되면', () => {
     });
 
     beforeEach(function(done) {   
+        /*
         spyOn($, 'getJSON').and.callFake(function(url, callback) {
             callback({sharePrice: 20.18});
         });
+        */
+
+        var fetchRequest;
+        xhr = sinon.useFakeXMLHttpRequest();
+        xhr.onCreate = function (request) {
+            fetchRequest = request;
+        };
+
         stock.fetch(null, done);
+
+        fetchRequest.respond(
+            200,
+            { "Content-Type": "application/json" },
+            '{ "sharePrice": 20.18}'
+        );
+    });
+
+    afterEach(() => {
+        xhr.restore();
     });
 
     it('주가를 업데이트해야한다', (done) => {
