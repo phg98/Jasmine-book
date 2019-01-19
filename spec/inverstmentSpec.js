a8ec97fba47f983925c451729795a549f160badc
Harry Park <hg98.park@hanwha.com>
2019-01-19 13:57:21 +0900

    #2 add 'beforeEach()

--- spec/inverstmentSpec.js
+++ spec/inverstmentSpec.js
@@ -1,7 +1,28 @@
 describe('investment', () => {
+    var stock, investment;
+
+    beforeEach(() => {        
+        stock = new Stock();
+        investment = new Investment({
+            stock: stock,
+            shares: 100,
+            sharePrice: 20
+        });
+
+    });
     it('should have stock', () => {
-        var stock = new Stock();
-        var investment = new Investment(stock);
         expect(investment.stock).toBe(stock);
+    });
+
+    it('should have shares', () => {
+        expect(investment.shares).toEqual(100);
+    });
+
+    it('should have share price', () => {
+        expect(investment.sharePrice).toEqual(20);
+    });
+
+    it('should have cost', () => {
+        expect(investment.cost).toEqual(2000);
     });
 })
