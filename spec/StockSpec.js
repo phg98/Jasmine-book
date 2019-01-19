6f3f983f9c37187e556184c533fdeb8b273d2f34
Harry Park <hg98.park@hanwha.com>
2019-01-20 00:21:47 +0900

    #8 simple Spy

--- spec/StockSpec.js
+++ spec/StockSpec.js
@@ -6,11 +6,14 @@ describe('주식이 조회되면', () => {
     });
 
     beforeEach(function(done) {   
+        spyOn($, 'getJSON').and.callFake(function(url, callback) {
+            callback({sharePrice: 20.18});
+        });
         stock.fetch(null, done);
     });
 
     it('주가를 업데이트해야한다', (done) => {
-        expect(stock.fetched).toEqual(1);
+        //expect(stock.fetched).toEqual(1);
         expect(stock.sharePrice).toEqual(20.18);
         done();
     });
