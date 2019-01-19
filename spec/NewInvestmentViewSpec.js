4ce7498ad8a775f2d391dce90ef0d618eff85a55
Harry Park <hg98.park@hanwha.com>
2019-01-19 17:14:25 +0900

    #3 loadFixtures()

---
+++ spec/NewInvestmentViewSpec.js
@@ -0,0 +1,25 @@
+describe('NewInvestmentView', () => {
+    beforeEach(() => {
+        loadFixtures('NewInverstmentView.html');
+    });
+    
+    it('should input stock code', () => {
+        
+    });
+
+    it('should input buy share count', () => {
+        
+    });
+
+    it('매수단가를 입력받아야 한다.', () => {
+        
+    });
+
+    /*
+    afterEach(() => {
+        $('#my-form').remove();
+    });
+    */
+});
+
+//"C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" --allow-file-access-from-files
